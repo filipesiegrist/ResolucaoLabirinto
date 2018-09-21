f765a9741407dac91801f075704d48d488f8d60e
filipesiegrist <filipebrasil8@gmail.com>
2018-09-21 16:06:14 -0300

    Comecei o mapa do labirinto

--- Sistemas_inteligentes/Solver.py
+++ Sistemas_inteligentes/Solver.py
@@ -26,19 +26,100 @@ import spade
 import time
 from random import randint
 
-class Solver(spade.Agent.Agent):
+#Classe que guarda o mapa do labirinto
+class Mapa:
+	#position
 	#vetor com coordenadas [0] - x
 	#                      [1] - y
-	global position
-	position = []
+	
+	#path
 	#vetor com o caminho percorrido
 	# Posição:
 	# 0	c
 	# 1	d
 	# 2	b
 	# 3	e
-	global path 
-	path = []
+	
+	#nodes
+	#Lista dos nodos percorridos, ex:
+	# [0,0] , [1,0] , [1,1]
+	
+	def __init__(self):
+		#posicao atual no labirinto
+		self.__position = [0,0]
+		#direcoes que foram percorridas
+		self.__path = []
+		#todas as coordenadas dos nodos percorridos
+		self.__nodes = []
+		#adiciona a posicao inicial
+		self.__nodes.append([0,0])
+	
+	#Funcao que efetua o movimento e atualiza as variaveis:
+	#	path
+	#	position
+	#	nodes
+	#Recebe Numero de 0-3 seguindo o padrao acima
+	def registraMovimento(self,numdir):
+		referencia = ['c','d','b','e']
+		
+		#adiciona o numero em path
+		self.__path.append(numdir)
+		
+		#atualiza as coordenadas:
+		if referencia[numdir] == 'c':
+			#y++
+			self.__position[1] += 1
+		elif referencia[numdir] == 'd':
+			#x++
+			self.__position[0] += 1
+		elif referencia[numdir] == 'b':
+			#y--
+			self.__position[1] -= 1
+		elif referencia[numdir] == 'e':
+			#x--
+			self.__position[0] -= 1
+			
+		
+		#adiciona a nova coordenada na lista de nodos
+		self.__nodes.append(self.__position)
+	
+	#retorna a posicao
+	def getPosition(self):
+		return self.__position
+		
+	#retorna as direcoes percorridas
+	def getPath(self):
+		return self.__path
+	
+	#retorna a lista de nodos percorridos
+	def getNodeList(self):
+		return self.__nodes
+		
+	def imprime(self):
+		print "----------Mapa do Labirinto-----------"
+		print "Posicao atual: " , self.__position
+		print "Caminho percorrido: " , self.__path
+		print "Nodos percorridos: " , self.__nodes
+		print "-------------Fim do Mapa---------------"
+
+	#define qual a proxima posicao que deve ir
+	#recebe string com as posicoes possiveis no estilo "[c, d, b, e]"
+	#retorna numero de 0-3 com a acao
+	def proxDirecao(self,posicoes):
+		acoes = ["0","cima","0","0","dir","0","0","baixo","0","0","esq"]
+		#escolhe acao nova enquanto der caminho
+		acao = randint(0,3)
+		while posicoes[acao*3 +1] != '1':
+			acao = randint(0,3)
+			#print "Iria para " , acoes[acao] 
+		print "Movendo para " , acoes[acao*3+1]
+		return acao
+
+class Solver(spade.Agent.Agent):
+	#Classe global, que é o mapa do labirinto
+	global MazeMap
+	MazeMap = Mapa()
+	
 	
 	#Filipe
 	#Recebe inform com posicoes dispniveis
@@ -48,20 +129,52 @@ class Solver(spade.Agent.Agent):
 	class Move(spade.Behaviour.Behaviour):
 		def _process(self):
 			#para ser ativado vai receber uma mensagem com as posicoes disponiveis
+
+			#ordem das acoes
+			acoes = ["cima","dir","baixo","esq"]
+			
+			print "Comportamento Move Iniciado"
+			
+			msgrecebida = self._receive(True)
+			
+			#vetor dizendo onde pode mover 0 é parede e 1 é caminho
+			posicoes = msgrecebida.getContent()
+			
+			print "mensagem recebida pelo move: " , posicoes
+			
+			#Recebe qual a proxima acao a ser feita das as posicoes
+			acao = MazeMap.proxDirecao(posicoes)
+			
+			#-----------<escolha da acao a ser feita>-------------
+			
+			#escolhe acao nova enquanto der caminho
+			# acao = randint(0,3)*3 +1
+			# while posicoes[acao] != '1':
+				# acao = randint(0,3)*+1
+				# #print "Iria para " , acoes[acao] 
+			
+			# print "Movendo para " , acoes[acao]
+			#-------------</escolhadeacaoaserfeita>-------------
 			
 			
+			#Envia uma mensagem de movimento para o labirinto
+			msgmove = spade.ACLMessage.ACLMessage()
+			msgmove.setPerformative("subscribe")
+			msgmove.addReceiver(spade.AID.aid("tabuleiro@127.0.0.1",["xmpp://tabuleiro@127.0.0.1"]))
+			msgmove.setContent(acoes[acao])            
+			self.myAgent.send(msgmove)
+			print "enfaggot"
+			#Nao precisa receber a resposta pq sabemos que sera automaticamente sucesso
 			
-			print "movendo"
-			#envia request com posicoes disponiveis
-			msg = spade.ACLMessage.ACLMessage()
-			msg.setPerformative("request")
-			msg.addReceiver(spade.AID.aid("tabuleiro@127.0.0.1",["xmpp://tabuleiro@127.0.0.1"]))
-			msg.setSender('solver')
-			#acao e a posicao aleatoria
-			acoes = ['c','b','e','d']
-			acao = acoes[randint(0,3)]
-			msg.setContent(acao)         
-			self.myAgent.send(msg)
+			#normaliza os numeros das direcoes para encaixarem de 0-3
+			movimento = acao
+			#atualiza o mapa
+			MazeMap.registraMovimento(movimento)
+			MazeMap.imprime()
+			#print "Estou na coordenada " , MazeMap.getPosition()
+			
+			#Remove esse comportamento
+			self.myAgent.removeBehaviour(self.myAgent.Move())
 		
 	#Daniel. 
 	#Recebe "inform" com 'true' ou 'false' dizendo se chegou no objetivo
@@ -75,6 +188,7 @@ class Solver(spade.Agent.Agent):
 	#Manda mensagem de criar e recebe essa mesma mensagem
 	#
 	class StartAction(spade.Behaviour.OneShotBehaviour):
+		#Adiciona comportamento do move
 		def _process(self):
 			
 			msgcria = spade.ACLMessage.ACLMessage()
@@ -88,13 +202,28 @@ class Solver(spade.Agent.Agent):
 			msgrecebida = self._receive(True);
 			
 			#se a mensagem for valida:
-			if len(msg.getSender().getName()) > 0:
-				print "mensagem de 'criar com sucesso' recebida"
-				#envia mensagem pedindo as posicoes
+			if len(msgrecebida.getSender().getName()) > 0:
+				print "mensagem de 'criar com sucesso' recebida: " , msgrecebida.getContent()
+				
+				#Cria template do Move
+				
+				#adiciona um template de mensagem para receber posições de movimento
+				templatePos = spade.Behaviour.ACLTemplate()
+				templatePos.setPerformative("inform")
+				templatePos.setSender(spade.AID.aid("tabuleiro@127.0.0.1",["xmpp://tabuleiro@127.0.0.1"]))
+				tPos = spade.Behaviour.MessageTemplate(templatePos)
+				
+				#adiciona comportamento de move
+				self.myAgent.addBehaviour(self.myAgent.Move(),tPos)
+				
+				
+				#Envia mensagem pedindo as posicoes disponiveis
 				msgsucessores = spade.ACLMessage.ACLMessage()
 				msgsucessores.setPerformative("request")
 				msgsucessores.addReceiver(spade.AID.aid("tabuleiro@127.0.0.1",["xmpp://tabuleiro@127.0.0.1"]))
-				msgcria.setContent('sucessores')
+				msgsucessores.setContent('sucessores')          
+				self.myAgent.send(msgsucessores)
+				print "mensagem dos sucessores enviada"
 			
 	class Propose(spade.Behaviour.Behaviour):
 		global a
@@ -108,6 +237,7 @@ class Solver(spade.Agent.Agent):
 		templateCria = spade.Behaviour.ACLTemplate()
 		templateCria.setPerformative("inform")
 		templateCria.setContent("criar")
+		templateCria.setSender(spade.AID.aid("tabuleiro@127.0.0.1",["xmpp://tabuleiro@127.0.0.1"]))
 		tCria = spade.Behaviour.MessageTemplate(templateCria)
 		
 		#adiciona o comportamento de começar o labirinto com um template de mensagem pra receber criacao
@@ -117,14 +247,13 @@ class Solver(spade.Agent.Agent):
 		#--------------Comportamento VerifyPosition------------------------------#
 
 		#adiciona um template de mensagem para receber posições de movimento
-		templatePos = spade.Behaviour.ACLTemplate()
-		templatePos.setPerformative("inform")
-		templatePos.setSender(spade.AID.aid("tabuleiro@127.0.0.1",["xmpp://tabuleiro@127.0.0.1"]))
-        templatePos.setContent("criar")
-		tPos = spade.Behaviour.MessageTemplate(templatePos)
+		#templatePos = spade.Behaviour.ACLTemplate()
+		#templatePos.setPerformative("inform")
+		#templatePos.setContent("criar")
+		#tPos = spade.Behaviour.MessageTemplate(templatePos)
 		
 		#adiciona comportamento de move
-		self.addBehaviour(self.Move(),tPos)
+		#self.addBehaviour(self.Move(),tPos)
 
 #testando a classe
 
